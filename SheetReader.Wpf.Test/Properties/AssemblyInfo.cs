d7af6107230e44bd177ac5b299ea0caf0cdc4ca8
Simon Mourier <smourier@users.noreply.github.com>
2025-01-04 14:54:06 +0100

    updated copyrights

--- SheetReader.Wpf.Test/Properties/AssemblyInfo.cs
+++ SheetReader.Wpf.Test/Properties/AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System.Runtime.Versioning;
 [assembly: AssemblyDescription("Sheet Reader Wpf Test")]
 [assembly: AssemblyCompany("Simon Mourier")]
 [assembly: AssemblyProduct("Sheet Reader")]
-[assembly: AssemblyCopyright("Copyright (C) 2021-2024 Simon Mourier. All rights reserved.")]
+[assembly: AssemblyCopyright("Copyright (C) 2021-2025 Simon Mourier. All rights reserved.")]
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("a51e10cf-4a86-4102-a90e-b2dda115e7a7")]
